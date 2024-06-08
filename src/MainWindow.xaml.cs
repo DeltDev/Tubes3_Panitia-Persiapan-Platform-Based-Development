3969c21e018155def6f818cb9552b24b90d33893
DeltDev <deltenc88@gmail.com>
2024-06-08 18:08:15 +0700

    feat: combine to gui (not fixed)

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using SixLabors.ImageSharp;
 namespace src
 {
     /// <summary>
@@ -24,7 +25,7 @@ namespace src
     public partial class MainWindow : Window
     {
 
-        private string chosenMethod;
+        private string chosenMethod; //method yang dipilih
         private string chosenIMG;
         private TextBlock chosenMethodLBL;
         private TextBlock EstTimeLBL;
@@ -44,7 +45,8 @@ namespace src
         {
             try
             {
-                string tempPath = "./../../../../temp";
+                string directory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string tempPath = System.IO.Path.Combine(directory, "./../../../../temp");
                 var files = Directory.GetFiles(tempPath);
 
                 foreach (var file in files)
@@ -97,14 +99,65 @@ namespace src
         }
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
-        {
+        {   string imgSelectedName = GetImageFilename(InputImage); //directory image asli yang didapat
+
             //tombol buat mulai search
             if (chosenMethod == "" || chosenMethod == null)
             {
                 MessageBox.Show("Anda belum memilih metode pencarian!\nSilakan pilih metode pencarian.", "Belum pilih metode pencarian", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
+            } else if(imgSelectedName == null || imgSelectedName == "")
+            {
+                MessageBox.Show("Anda belum memilih citra!\nSilakan pilih citra terlebih dahulu.", "Belum pilih citra", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
             }
-            chosenMethodLBL.Text = chosenMethod;
+            chosenMethodLBL.Text = chosenMethod; //method yang dipilih
+            //chosenMethod: metode yang dipilih
+            string imageName = System.IO.Path.GetFileName(imgSelectedName); //hanya nama filenya
+            string directory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);//pindahkan directory
+            string tempPath = System.IO.Path.Combine(directory, "./../../../../temp", imageName);//gabungkan path temp dengan nama file
+                                                                                                 //MessageBox.Show($"The image filename is: {tempPath}");
+
+
+            //algoritma utama saat searching
+            string connectionString = $"Server=localhost;Database=stima3;Uid=root;Pwd=;";
+            DatabaseManager dbManager = new DatabaseManager(connectionString);
+            AlgoritmaPatternMatching alg;
+            if (chosenMethod == "Knuth-Morris-Pratt")
+            {
+                alg = new KMP();
+            }
+            else
+            {
+                alg = new BM();
+            }
+            // Sidik jari db
+            List<string> list_sidik_jari_db = dbManager.getAllSidikJari();
+            // Mulai pencarian
+            KeyValuePair<string, double> output = PencocokSidikJari.MulaiPencocokan(tempPath, list_sidik_jari_db, alg);
+            // Output hasil
+            if (output.Value > 0.6)
+            {
+                string sidik_jari_akhir = output.Key;
+                string getname = dbManager.getNameFromSidikJari(sidik_jari_akhir);
+
+                List<string> data = dbManager.getAllNamaFromBiodata();
+                string realname = Regex.ResultText(data, getname);
+                MessageBox.Show($"Tingkat kemiripan: {output.Value}");
+                dbManager.printDataFromName(realname);
+            }
+            else
+            {
+                MessageBox.Show("Tidak ada sidik jari dengan tingkat kemiripan diatas 0,6.");
+            }
+        }
+        private string GetImageFilename(System.Windows.Controls.Image image)
+        {
+            if (image.Source is BitmapImage bitmapImage)
+            {
+                return bitmapImage.UriSource.LocalPath;
+            }
+            return null;
         }
     }
 }
