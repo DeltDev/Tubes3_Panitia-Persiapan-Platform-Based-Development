6884d85fb52e9d75581e5f7c847c813aa380bf22
DeltDev <deltenc88@gmail.com>
2024-06-09 18:32:58 +0700

    chore: exception handling and clear image removal

--- src/Config.cs
+++ src/Config.cs
@@ -2,5 +2,5 @@ public class Config
 {
     public static string Database = "stima3";
     public static string Uid = "root";
-    public static string Password = "";
+    public static string Password = "Than0s123";
 }
